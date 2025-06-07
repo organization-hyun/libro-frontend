ed60d23547394d9f921bd2c67523df1195e7647f
hyunjuuun <hyunjun86120@gmail.com>
2025-06-07 19:38:12 +0900

    로그인 api 호출 정의된 config 설정 이용

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
 import { LoginRequest, LoginResponse } from '../types/auth';
-import { api } from './axios';
+import api from '@api/config';
 
 export const login = async (credentials: LoginRequest): Promise<LoginResponse> => {
   const response = await api.post<LoginResponse>('/auth/login', credentials);
